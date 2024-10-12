aae43bef9aa07a11d283b940c3281396f053e29f
indahtangdililing <indahtangdililing@gmail.com>
2024-10-13 00:19:34 +0700

    indah: FileHandler

---
+++ src/matrixUtils.java
@@ -0,0 +1,29 @@
+public class matrixUtils {
+    
+    // Fungsi untuk menukar dua baris dalam matriks
+    public static void swap_rows(double[][] mat, int row1, int row2) {
+        for (int i = 0; i < mat[0].length; i++) { // mat[0].length = jumlah kolom
+            double temp = mat[row1][i];
+            mat[row1][i] = mat[row2][i];
+            mat[row2][i] = temp;
+        }
+    }   
+
+    // Fungsi untuk mengalikan satu baris dengan skalar
+    public static void multiply_row(double[][] mat, int row, double scal) {
+        for (int i = 0; i < mat[0].length; i++) {
+            mat[row][i] *= scal;
+        }
+    }
+
+    // Fungsi untuk menambahkan satu baris ke baris lain dengan skalar
+    public static void add_other_row(double[][] mat, int row, int added_row, double scal) {
+        for (int i = 0; i < mat[0].length; i++) {
+            mat[row][i] += scal * mat[added_row][i];
+        }
+    }
+}
+
+
+// FileHandler m = new FileHandler("path/to/matrix_file.txt");
+// matrixUtils.swap_rows(m.mat, 0, 1);
