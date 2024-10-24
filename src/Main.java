31d2a4ef57056d5a6f87e09f91565286a8b94ac4
indahtangdililing <indahtangdililing@gmail.com>
2024-10-24 23:10:22 +0700

    Main baru

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,20 @@ import java.util.Scanner;
 
 public class Main {
     public static boolean keluar = false;
+
     public static void main(String[] args) {
-        while (!keluar){
-            System.out.println("MAIN");
+        System.out.println("=========================");
+        System.out.println("|        CEPALTA        |");
+        System.out.println("=========================");
+        
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        while (!keluar) {
+            System.out.println("===========MAIN==========");
             System.out.println("1. Sistem Persamaan Linier");
             System.out.println("2. Determinan");
             System.out.println("3. Matriks Balikan");
@@ -14,7 +25,7 @@ public class Main {
             System.out.println("6. Regresi");
             System.out.println("0. KELUAR");
             Scanner input = new Scanner(System.in);
-            while (!input.hasNextInt() && input.nextInt()<=6) {
+            while (!input.hasNextInt() && input.nextInt() <= 6) {
                 System.out.println("Harap masukkan angka.");
                 input.next();
             }
@@ -42,9 +53,6 @@ public class Main {
                     keluar = true;
                     break;
             }
-
         }
     }
-
-    
 }
